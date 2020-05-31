8c096e46c83ac30c1af97525907f7636d57b034b
DrBeta <>
2020-05-31 13:56:36 -0500

    Added another example and added comments

--- dilbert/Example.py
+++ dilbert/Example.py
@@ -3,6 +3,12 @@ import Dilbert
 import datetime
 from PIL import Image
 import io
+#Fetches today's Dilbert
 response = requests.get(Dilbert.dilbert_from_date(datetime.datetime.now()))
+dilbertimg = Image.open(io.BytesIO(response.content))
+dilbertimg.show()
+
+#Fectches Dilbert from 35 days ago
+response = requests.get(Dilbert.dilbert_days_ago(365))
 dilbertimg = Image.open(io.BytesIO(response.content))
 dilbertimg.show()
